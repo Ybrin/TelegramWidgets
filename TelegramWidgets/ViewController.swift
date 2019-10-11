9e53cd3eab9744e3df09ddbe0a6dda244821733e
Koray Koska <koray@koska.at>
2019-10-11 22:27:31 +0200

    Initial Commit

---
+++ TelegramWidgets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TelegramWidgets
+//
+//  Created by Koray Koska on 11.10.19.
+//  Copyright © 2019 Koray Koska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
